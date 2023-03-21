e157074882e363b4b6ef07046970f0927b12d080
ThilNguyen <111734383+ThilNguyen@users.noreply.github.com>
2023-03-21 17:01:06 +0100

    Add files via upload

---
+++ TicTacToe/Klassen/Game.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        private Board board;
+        public char currentPlayer;
+
+        public Game()
+        {
+            board = new Board();
+            currentPlayer = 'X';
+        }
+
+        public void Play()
+        {
+            board.Draw();
+
+            while (!board.IsGameOver())
+            {
+                try
+                {
+                    MakeMove();
+                if (board.HasWinner())
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Player {currentPlayer} wins!");
+                        
+                }
+                else
+                {
+                    currentPlayer = currentPlayer == 'X' ? 'O' : 'X'; // switch player
+                }
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private void MakeMove()
+        {
+            Console.WriteLine($"Player {currentPlayer}, it's your turn.");
+
+            bool validMove = false;
+            do
+            {
+                //int row = int.Parse(Console.ReadLine()) - 1;
+                //int row = Console.Read();
+                int row = GetMove("row");
+                int column = GetMove("column");
+
+                try
+                {
+                    board.MakeMove(currentPlayer, row, column);
+                    validMove = true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            } while (!validMove);
+
+            board.Draw();
+        }
+
+        public int GetMove(string type)
+        {
+            int number;
+            do
+            {
+                Console.Write($"Enter the {type} (1-3): ");
+                string input = Console.ReadLine();
+                int.TryParse(input, out number);
+            } while (number < 1 || number > 3);
+            return number;
+        }
+    }
+}
---
+++ TicTacToe/README.md
@@ -0,0 +1,13 @@
+# TicTacToe
+TicTacToe Projekt V320
+
+Sprintziel 14.03.2023
+
+Wir wollen Azure DevOps und GitHub eingerichtet haben.
+
+Retrospektive
+
+Wir konnten in unserem ersten Sprint die Userstorys schreiben, die Produktvision, die verschiedenen Diagramme sowie auch angefangen Code zu schreiben. Wir haben unser Taskboard organisiert und angepasst. 
+
+Die Gruppe denkt, dass man einiges besser hätte machen können. Man hätte bereits mehr Funktionen zum Spiel hinzufügen können. Auch hätten wir uns besser mit den UserStorys befassen können und diese logischer gestallten können.    
+
