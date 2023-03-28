e95b5aaed7e1ea84f504d253ef4d1e149f70fe39
LivioPiccolotto <111734364+LivioPiccolotto@users.noreply.github.com>
2023-03-28 16:44:04 +0200

    mementopattern

---
+++ TicTacToe/Klassen/Memento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+
+    public class Memento
+    {
+        public char[,] BoardState { get; }
+
+        public Memento(char[,] boardState)
+        {
+            BoardState = (char[,])boardState.Clone();
+        }
+    }
+}
+
