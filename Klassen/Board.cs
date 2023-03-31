fd18d678c92862457ad37fc2aaba121d0243c7a4
ThilNguyen <111734383+ThilNguyen@users.noreply.github.com>
2023-03-31 19:55:25 +0200

    Add files via upload

---
+++ Klassen/Board.cs
@@ -0,0 +1,137 @@
+﻿namespace TicTacToe
+{
+        
+    public class Board
+    {
+        private char[,] board = new char[3, 3];
+
+        public Board()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initialize Methode initialisierst einen 2D-Array mit Leerzeichen
+        /// </summary>
+        private void Initialize()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Überprüft, ob das feld besetzt ist und macht einen Spielzug
+        /// </summary>
+        /// <param name="player">Spieler</param>
+        /// <param name="row">Reihe beim Board, um zu spielen</param>
+        /// <param name="column">Spalte beim Board</param>
+        /// <exception cref="InvalidOperationException">Fehlermeldung, falls Feld besetzt ist</exception>
+        public void MakeMove(char player, int row, int column)
+        {
+            row -= 1;
+            column -= 1;
+            int colIndex = column;
+            if (board[row, column] != ' ')
+            {
+                throw new InvalidOperationException("Dieses Feld ist schon besetzt!");
+            }
+
+            board[row, column] = player;
+        }
+
+
+        /// <summary>
+        /// Zeichnet das Zeichen vom Spieler ins Spielfeld
+        /// </summary>
+        /// <param name="row">Reihe</param>
+        /// <param name="column">Spalte</param>
+        /// <returns>gibt das Zeichen vom Spieler zurück</returns>
+        public char GetSymbol(int row, int column)
+        {
+            return board[row, column];
+        }
+
+        /// <summary>
+        /// Zeichnet das Spielfeld auf der Konsole
+        /// </summary>
+        public void Draw()
+        {
+            Console.Clear();
+            Console.WriteLine("    1     2     3   ");
+            Console.WriteLine("1   {0}  |  {1}  |  {2} ", GetSymbol(0, 0), GetSymbol(0, 1), GetSymbol(0, 2));
+            Console.WriteLine("   ----+-----+----");
+            Console.WriteLine("2   {0}  |  {1}  |  {2} ", GetSymbol(1, 0), GetSymbol(1, 1), GetSymbol(1, 2));
+            Console.WriteLine("   ----+-----+----");
+            Console.WriteLine("3   {0}  |  {1}  |  {2} ", GetSymbol(2, 0), GetSymbol(2, 1), GetSymbol(2, 2));
+        }
+
+        /// <summary>
+        /// Überprüft, ob es einen Gewinner gibt mit Gewinnkombinationen
+        /// </summary>
+        /// <returns>Falls es einen Gewinner gibt ist es "true" zurück, falls nicht dann "false"</returns>
+        public bool HasWinner()
+        {
+            // Check rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return true;
+            }
+
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        /// <summary>
+        /// Überprüft, ob es einen Gewinner gibt oder, ob alle Felder besetzt sind
+        /// </summary>
+        /// <returns>Wenn das Spiel vorbei ist, gibt die Methode "true" zurück, andernfalls "false"</returns>
+        public bool IsGameOver()
+        {
+            if (HasWinner())
+            {
+                return true;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Klassen/Game.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        private Board board;
+        public char currentPlayer;
+
+        public Game()
+        {
+            board = new Board(); 
+            currentPlayer = 'X';
+        }
+
+        /// <summary>
+        /// Zeichnet das Spielbrett immer neu, bis das Spiel vorbei ist gibt und verwendet Methoden, die in der Klasse Game definiert sind
+        /// Wenn das Spiel vorbei ist, zeichnet es das Spielbrett erneut, um den Spielstand zu zeigen
+        /// </summary>
+        public void Play()
+        {
+            while (!board.IsGameOver())
+            {
+                board.Draw();
+                try
+                {
+                    MakeMove();
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            board.Draw();
+            DrawWinner();
+            HandleInput();
+        }
+
+
+        /// <summary>
+        /// Verarbeitet die Eingabe des Spielers, wenn das Spiel vorbei ist
+        /// Wenn "neu" eingegeben wird, wird ein neues Spiel gestartet
+        /// Wenn "ende" eingegeben wird, wird das Spiel beendet
+        /// </summary>
+        public void HandleInput()
+        {
+            while (true)
+            {
+                Console.Write("Gebe eines dieser Befehle ein (neu oder ende): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "neu")
+                {
+                    board = new Board();
+                    currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+                    Play();
+                }
+                else if (input == "ende")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Ungültiger Befehl. Versuche es erneut.");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Ermittelt den Gewinner und gibt ihn als Nachricht aus
+        /// Falls es keine Gewinnner gibt, wird ausgegeben, dass es unentschieden ist.
+        /// </summary>
+        private void DrawWinner()
+        {
+            if (board.HasWinner())
+            {
+                currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+                Console.WriteLine($"Spieler {currentPlayer} hat gewonnen!");
+            }
+            else
+            {
+                Console.WriteLine("Es ist ein Unentschieden!");
+            }
+        }
+
+
+        /// <summary>
+        /// Verarbeitet den Zug eines Spielers
+        /// Wenn der Zug gültig ist, wird er auf dem Spielfeld platziert
+        /// Falls es keine gültige Eingabe ist, wird eine Fehlermeldung angezeigt
+        /// </summary>
+        private void MakeMove()
+        {
+            Console.WriteLine($"Spieler {currentPlayer}, du bist dran!");
+
+            bool validMove = false;
+            do
+            {
+                int row = GetMove("Zeile");
+                int column = GetMove("Spalte");
+
+                try
+                {
+                    board.MakeMove(currentPlayer, row, column);
+                    validMove = true;
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            } while (!validMove);
+
+            currentPlayer = currentPlayer == 'X' ? 'O' : 'X'; 
+        }
+
+ 
+        /// <summary>
+        /// Schleife, um eine gültige Eingabe vom Spieler zu erhalten
+        /// </summary>
+        /// <param name="type">"row" oder "column" vom Spielfeld</param>
+        /// <returns>Die eingegebene Zahl wird zurückgegeben</returns>
+        public int GetMove(string type)
+        {
+            int number;
+            do
+            {
+                Console.Write($"Gebe die {type} ein (1-3): ");
+                string input = Console.ReadLine();
+                int.TryParse(input, out number);
+            } while (number < 1 || number > 3);
+            return number;
+        }
+    }
+}
